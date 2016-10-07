67752eafa50299bb765ee8d2418a3c249e3c4618
Artur Ashyrov <artur.ashyrov@gmail.com>
2016-10-07 19:22:26 +0300

    Almost working solution

--- sample-service/src/main/java/se/tain/ScopedConfigServiceBootstrapConfiguration.java
+++ sample-service/src/main/java/se/tain/ScopedConfigServiceBootstrapConfiguration.java
@@ -5,8 +5,17 @@ import org.springframework.cloud.config.client.ConfigServicePropertySourceLocato
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertySource;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
 
 @Configuration
 // https://github.com/spring-cloud/spring-cloud-config/issues/177
@@ -27,13 +36,30 @@ public class ScopedConfigServiceBootstrapConfiguration implements EnvironmentAwa
         ConfigServicePropertySourceLocator configServicePropertySourceLocator =  new ConfigServicePropertySourceLocator(clientProperties) {
             @Override
             public PropertySource<?> locate( Environment environment ) {
-                PropertySource<?> propertySource = super.locate( environment );
-                /*
-                CompositePropertySource compositePropertySource = new CompositePropertySource( "scopedCloudConfigsPropertySource" );
-                compositePropertySource.addPropertySource( new EnumerablePropertySource<>() {
-                } );
-*/
-                return propertySource;
+                CompositePropertySource cloudPropertySource = ( CompositePropertySource ) super.locate( environment );
+                return new CompositePropertySource( "scopedCloudConfigsPropertySource" ) {
+                    @Override
+                    public Object getProperty(String name) {
+                        String op = OperatorContext.getCurrentOperator();
+                        String opScopedName = null;
+                        if ( StringUtils.hasText(op)) {
+                            opScopedName = format("%s.%s", op, name);
+                        }
+                        return opScopedName != null && cloudPropertySource.containsProperty( opScopedName ) ? cloudPropertySource.getProperty(opScopedName) : cloudPropertySource.getProperty(name);
+                    }
+
+                    @Override
+                    public String[] getPropertyNames() {
+                        String op = OperatorContext.getCurrentOperator();
+                        String[] propertyNames = cloudPropertySource.getPropertyNames();
+                        if(StringUtils.hasText(op)){
+                            Stream<String> stream = Arrays.stream(propertyNames);
+                            List<String> propNamesFiltered = stream.map( s -> s.replace(format("%s.", op), "")).collect( Collectors.toList());
+                            propertyNames = propNamesFiltered.toArray(new String[propNamesFiltered.size()]);
+                        }
+                        return propertyNames;
+                    }
+                };
             }
         };
         return configServicePropertySourceLocator;
